db523bf4ab9d72e340d016c6443d52fd8e1a8e7e
Ravi Malik <ravimalik2364@gmail.com>
2018-03-11 22:13:21 -0400

    Import front-end added.

---
+++ media/js/import.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+	$("form").submit(function () {
+		var data = $(this).serializeArray();
+		var url = $(this).attr("action");
+	
+		$.post(url, data, function (val) {
+			if (val.status == "success") {
+				window.location = "/alumni";
+			}
+			else	
+				alert("Incorrect data.");
+		});
+	
+		return false;
+	});
+	
+	$("#spinner").hide();
+	
+	Dropzone.options.filedrop = {
+	  init: function () {
+		this.on("addedfile",function () {
+			$("#spinner").show();
+		});
+	  
+		this.on("error", function () {
+			$("#spinner").hide();
+		});
+	  
+		this.on("complete", function (file) {
+		  if (this.getUploadingFiles().length === 0 && this.getQueuedFiles().length === 0) {
+			$("#spinner").hide();
+		  }
+		});
+	  }
+	};
+});
