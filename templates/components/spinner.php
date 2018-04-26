b8595b5566bf3900ed3f1804114111a5bbf705e0
Ravi Malik <ravimalik2364@gmail.com>
2018-04-26 00:25:33 -0400

    Sample CSV format added.

---
+++ templates/components/spinner.php
@@ -0,0 +1,11 @@
+<style>
+	#cover {
+		background: url("<?=Helper::url('/media/spinner.gif')?>") no-repeat scroll center center #FFF;
+		position: absolute;
+		height: 100%;
+		width: 100%;
+		z-index: 99999;
+	}
+</style>
+
+<div id="cover"></div>
